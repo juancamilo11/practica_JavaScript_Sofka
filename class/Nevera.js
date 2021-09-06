9dd0d2e5424e10b612a6a87b52b7a2cd3e105c17
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-06 11:09:08 -0500

    Clases de entidad terminadas

---
+++ class/Nevera.js
@@ -0,0 +1,39 @@
+
+
+class Nevera extends Electrodomestico {
+
+   constructor(procedencia,tipoConsumo,capacidad) {
+      super(procedencia, tipoConsumo);
+      this.capacidad = capacidad;
+      this.calcularPrecioFinal();
+      this.nombre = "Nevera";
+   }
+
+    get getCapacidad() {
+        return this.capacidad;
+    }
+
+    set setCapacidad(capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    calcularPrecioFinal(){
+        if(this.capacidad > 120) {
+            let resto = capacidad - 120;
+            resto /= 10;
+            resto = Math.floor(resto);
+
+            precioFinal += resto * (0.05 * precioFinal);
+
+            this.precioFinal += (int) (this.precioFinal * 0.3);
+        }
+    }
+
+    
+    toString() {
+        return "Nevera[" +
+                super.toString() + "\n" +
+                "capacidad=" + this.capacidad +
+                ']';
+    }
+}
---
+++ class/Televisor.js
@@ -0,0 +1,49 @@
+class Televisor extends Electrodomestico{
+
+   constructor(procedencia, tipoConsumo, tamanio, tdt) {
+      super(procedencia, tipoConsumo);
+      this.tamanio = tamanio;
+      this.tdt = tdt;
+      this.calcularPrecioFinal();
+      this.nombre = "Televisor";
+    }
+
+   get getTamanio() {
+      return this.tamanio;
+   }
+
+   set setTamanio(tamanio) {
+      this.tamanio = tamanio;
+    }
+
+   get isTdt() {
+      return this.tdt;
+   }
+
+   set setTdt(tdt) {
+      this.tdt = tdt;
+   }
+
+   calcularPrecioFinal() {
+      if(this.tamanio > 40) {
+         this.precioFinal += this.precioFinal * 0.3;
+      }
+      if(this.tdt) {
+         this.precioFinal += 250000;
+      }
+   }
+
+   toString() {
+      return "Televisor[" +
+            "nombre='" + this.nombre + '\'' +
+            ", precioFinal=" + this.precioFinal +
+            ", procedencia='" + this.procedencia + '\'' +
+            ", tipoConsumo=" + this.tipoConsumo +
+            ", tamanio=" + this.tamanio +
+            ", tdt=" + this.tdt +
+            ']';
+   }
+}
+
+
+
